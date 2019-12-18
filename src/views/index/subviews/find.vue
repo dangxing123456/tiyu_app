764df416dd82007b33ceb3ee9a3c2a19ccbf7b18
dangxing <1660141755@qq.com>
2019-12-18 17:26:21 +0800

    12

--- src/views/index/subviews/find.vue
+++ src/views/index/subviews/find.vue
@@ -110,6 +110,8 @@
         </van-tabs>
       </div>
     </div>
+
+    <!-- 组件 -->
     <popup ref="pop" v-model="currentValue"></popup>
     <van-popup v-model="shiliShow" round>
       <div class="shili">
